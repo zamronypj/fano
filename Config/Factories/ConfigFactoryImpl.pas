25dc0ad5ee5e2e7bc74007d99f2da456cc661dea
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-10-04 19:44:19 +0800

    create factory for config class

---
+++ Config/Factories/ConfigFactoryImpl.pas
@@ -0,0 +1,41 @@
+unit ConfigFactoryImpl;
+
+interface
+
+uses
+    ConfigFactoryIntf,
+    DependencyAwareIntf,
+    DependencyContainerIntf,
+    DependencyFactoryIntf,
+    FactoryImpl;
+
+type
+    {------------------------------------------------
+     factory class for TFanoConfig
+     @author Zamrony P. Juhara <zamronypj@yahoo.com>
+    -----------------------------------------------}
+    TFanoConfigFactory = class (TFactory, IWebConfigurationFactory, IDependencyFactory)
+    private
+        configFilename : string;
+    public
+        constructor create(const dc : IDependencyContainer; const configFile :string);
+        function build() : IDependencyAware; override;
+    end;
+
+implementation
+
+uses
+    ConfigImpl;
+
+    constructor TFanoConfigFactory.create(const dc : IDependencyContainer; const configFile : string);
+    begin
+        inherited create(dc);
+        configFilename := configFile;
+    end;
+
+    function TFanoConfigFactory.build() : IDependencyAware;
+    begin
+        result := TFanoConfig.create(configFilename);
+    end;
+
+end.
