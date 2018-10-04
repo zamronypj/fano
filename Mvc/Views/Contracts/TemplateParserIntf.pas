6c6dcde8bc9f0a2eb9f65727b3e513de70df4491
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-10-04 19:34:58 +0800

    add template parser interface

---
+++ Mvc/Views/Contracts/TemplateParserIntf.pas
@@ -0,0 +1,23 @@
+unit TemplateParserIntf;
+
+interface
+
+uses
+    ViewParamsIntf;
+
+type
+    {------------------------------------------------
+     interface for any class having capability parse
+     template and replace variable placeholder with value
+     @author Zamrony P. Juhara <zamronypj@yahoo.com>
+    -----------------------------------------------}
+    ITemplateParser = interface
+        ['{06146BC7-5543-4FD1-956C-9FC47122EC70}']
+        function parse(
+            const templateStr : string;
+            const viewParams : IViewParameters
+        ) : string;
+    end;
+
+implementation
+end.
