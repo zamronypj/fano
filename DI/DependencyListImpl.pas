0547fdbec8bb2a5eae076b7ad5f41f57e4e2ae6b
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-10-09 12:20:03 +0800

    remove unused unit and add comment

--- DI/DependencyListImpl.pas
+++ DI/DependencyListImpl.pas
@@ -1,3 +1,10 @@
+{*!
+ * Fano Web Framework (https://fano.juhara.id)
+ *
+ * @link      https://github.com/zamronypj/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/zamronypj/fano/blob/master/LICENSE (GPL 2.0)
+ *}
 unit DependencyListImpl;
 
 interface
