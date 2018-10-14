e33fca51ac8ce56f1125bdc2f92058834e1085e7
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-10-14 09:59:17 +0800

    initial implementation of route regex

--- Router/RegexRouteListImpl.pas
+++ Router/RegexRouteListImpl.pas
@@ -2,6 +2,8 @@ unit RegexRouteListImpl;
 
 interface
 
+{$H+}
+
 uses
     RouteListIntf,
     HashListImpl,
@@ -60,6 +62,13 @@ const
     -------------------------------------------------*)
     ROUTE_DISPATCH_REGEX = '[^/]+';
 
+type
+
+    TRouteDataRec = record
+        placeholders: TArrayOfPlaceholders;
+        routeData : pointer;
+    end;
+    PRouteDataRec = ^TRouteDataRec;
 
     (*------------------------------------------------
       get placeholder variable original
@@ -93,14 +102,14 @@ const
     end;
 
     (*------------------------------------------------
-      replace original route pattern :
-        /name/{name:[a-zA-Z0-9\-\*\:]+}/{unitId}/edback
-
-      into route pattern ready for dispatch
-      /name/[^/]+/[^/]+/edback
-
-      See ROUTE_DISPATCH_REGEX constant
-    ---------------------------------------------------*)
+     * replace original route pattern :
+     *   /name/{name:[a-zA-Z0-9\-\*\:]+}/{unitId}/edback
+     *
+     * into route pattern ready for dispatch
+     * /name/[^/]+/[^/]+/edback
+     *
+     * See ROUTE_DISPATCH_REGEX constant
+     *---------------------------------------------------*)
     function TRegexRouteList.replacePlaceholderRegexWithDispatherRegex(
         const originalRouteWithRegex : string;
         const placeholders : TArrayOfPlaceholders
@@ -114,10 +123,10 @@ const
         );
         if (matched) then
         begin
-        for i := 0 to leng(matched)-1 do
-        begin
+            for i := 0 to leng(matched)-1 do
+            begin
 
-        end;
+            end;
         end;
     end;
 
@@ -132,15 +141,85 @@ const
         regex := nil;
     end;
 
+    (*!------------------------------------------------
+     * add route pattern to list
+     *-------------------------------------------------
+     * @param routeName original route as defined by app
+     *   /name/{name:[a-zA-Z0-9\-\*\:]+}/{unitId}/edback
+     * @param routeData, pointer to route data (such handler, etc)
+     * @return integer index of added item in list
+     *-------------------------------------------------
+     *
+     * If we have following input route name
+     *   /name/{name:[a-zA-Z0-9\-\*\:]+}/{unitId}/edback
+     * (1) we will extract placeholder data
+     *     into array of placeholder
+     *     [
+     *         {
+     *             varName : 'name'
+     *             optionalRegex : '[a-zA-Z0-9\-\*\:]+',
+     *         },
+     *         {
+     *             varName : 'unitId'
+     *             optionalRegex : ''
+     *         }
+     *     ]
+     * (2) replace route name into new regex string (transformed route name) as
+     *   /name/[^/]+/[^/]+/edback
+     * (3) store transformed route name into list
+     *---------------------------------------------------*)
     function TRegexRouteList.add(const routeName : string; const routeData : pointer) : integer;
     var replacedRoutePattern : string;
+        placeholders : TArrayOfPlaceholders;
     begin
-        replacedRoutePattern :=
+        placeholders := getPlaceholderFromOriginalRoute(routeName);
+        replacedRoutePattern := regex.replace();
+
         result := inherited add(routeName, routeData);
     end;
 
-    function TRegexRouteList.find(const routeName : string) : pointer;
+    function TRegexRouteList.combineRegexRoutes() : string;
+    var i, len : integer;
     begin
+        len := count();
+        for i:=0 to len-1 do
+        begin
+
+        end;
+    end;
+
+    (*------------------------------------------------
+     * match request uri with route list
+     *------------------------------------------------
+     * @param requestUri, original request uri
+     * @return routeData of matched route
+     *-------------------------------------------------
+     * For example, if we have following uri
+     *   /name/juhara/nice/edback
+     * and following registered route patterns
+     *  (1) /name/([^/]+)/([^/]+)/edback
+     *  (2) /article/([^/]+)/([^/]+)
+     *  (3) /articles/([^/]+)/([^/]+)
+     *
+     * (i) We will combine all registered route patterns in list into one
+     * string regex in form, so we can match all routes using only one
+     * regex matching call. (?:) is non capturing group so only inside parenthesis
+     * that will be captured
+     *
+     *  (?:
+     *  (/name/([^/]+)/([^/]+)/edback)|
+     *  (/article/([^/]+)/([^/]+)|
+     *  (/articles/([^/]+)/([^/]+)
+     *  )
+     *
+     * (ii) Based on matched group (the second non empty group),
+     * we can get index to the list of routes list.
+     * (iii)
+     *---------------------------------------------------*)
+    function TRegexRouteList.find(const requestUri : string) : pointer;
+    var combinedRegex : string;
+    begin
+        combinedRegex := combineRegexRoutes();
         result := hashes.find(routeName);
     end;
 
