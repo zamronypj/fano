3898a7e404fcf72c240b67f5f80b7c2738ed09ae
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-10-16 13:23:23 +0800

    add doc block comment

--- Mvc/Views/TemplateFileViewImpl.pas
+++ Mvc/Views/TemplateFileViewImpl.pas
@@ -1,6 +1,15 @@
+{*!
+ * Fano Web Framework (https://fano.juhara.id)
+ *
+ * @link      https://github.com/zamronypj/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/zamronypj/fano/blob/master/LICENSE (GPL 2.0)
+ *}
+
 unit TemplateFileViewImpl;
 
 interface
+
 {$H+}
 
 uses
--- Router/RegexRouteListImpl.pas
+++ Router/RegexRouteListImpl.pas
@@ -1,3 +1,11 @@
+{*!
+ * Fano Web Framework (https://fano.juhara.id)
+ *
+ * @link      https://github.com/zamronypj/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/zamronypj/fano/blob/master/LICENSE (GPL 2.0)
+ *}
+
 unit RegexRouteListImpl;
 
 interface
