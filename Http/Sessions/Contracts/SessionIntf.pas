8de7ae4d3a542350e6af6fb62d5d610126f5247a
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-10-27 23:28:10 +0800

    add session interface

---
+++ Http/Sessions/Contracts/SessionIntf.pas
@@ -0,0 +1,35 @@
+{*!
+ * Fano Web Framework (https://fano.juhara.id)
+ *
+ * @link      https://github.com/zamronypj/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/zamronypj/fano/blob/master/LICENSE (GPL 3.0)
+ *}
+
+unit SessionIntf;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+type
+
+    (*!------------------------------------------------
+     * interface for any class having capability to
+     * manage session data
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    ISession = interface
+        ['{3A456015-1398-461E-A272-AAFB67D5086F}']
+        function has(const key : string) : boolean;
+        function read(const key : string) : string;
+        function write(const key : string; const value : string) : ISession;
+    end;
+
+implementation
+
+
+
+end.
