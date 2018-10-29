94495f584463264b398764b1fc819f54f2ff73a6
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-10-29 18:34:55 +0800

    implement binary response

---
+++ Http/Response/BinaryResponseImpl.pas
@@ -0,0 +1,152 @@
+{*!
+ * Fano Web Framework (https://fano.juhara.id)
+ *
+ * @link      https://github.com/zamronypj/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/zamronypj/fano/blob/master/LICENSE (GPL 3.0)
+ *}
+
+unit BinaryResponseImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+    ResponseIntf,
+    ResponseStreamIntf,
+    HeadersIntf,
+    CloneableIntf;
+
+type
+    (*!------------------------------------------------
+     * base binary response class such image
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    TBinaryResponse = class(TInterfacedObject, IResponse, ICloneable)
+    private
+        httpHeaders : IHeaders;
+        contentType : string;
+        responseBody : IResponseStream;
+        procedure writeToStdOutput(const respBody : IResponseStream);
+    public
+        constructor create(
+            const hdrs : IHeaders;
+            const contentType : string;
+            const respBody : IResponseStream
+        );
+        destructor destroy(); override;
+
+        (*!------------------------------------
+         * get http headers instance
+         *-------------------------------------
+         * @return header instance
+         *-------------------------------------*)
+        function headers() : IHeaders;
+
+        function write() : IResponse;
+        function clone() : ICloneable;
+
+        (*!------------------------------------
+         * get response body
+         *-------------------------------------
+         * @return response body
+         *-------------------------------------*)
+        function body() : IResponseStream;
+    end;
+
+implementation
+
+uses
+
+    classes,
+    sysutils;
+
+const
+
+    BUFFER_SIZE = 8 * 1204;
+
+    constructor TBinaryResponse.create(
+        const hdrs : IHeaders;
+        const strContentType : string;
+        const respBody : IResponseStream
+    );
+    begin
+        httpHeaders := hdrs;
+        contentType := strContentType;
+        responseBody := respBody;
+    end;
+
+    destructor TBinaryResponse.destroy();
+    begin
+        inherited destroy();
+        httpHeaders := nil;
+        responseBody := nil;
+    end;
+
+    (*!------------------------------------
+     * get http headers instance
+     *-------------------------------------
+     * @return header instance
+     *-------------------------------------*)
+    function TBinaryResponse.headers() : IHeaders;
+    begin
+        result := httpHeaders;
+    end;
+
+    (*!------------------------------------
+     * read response body and output it to
+     * standard output
+     *-------------------------------------
+     * @param respBody response stream to output
+     *-------------------------------------*)
+    procedure TBinaryResponse.writeToStdOutput(const respBody : IResponseStream);
+    var numBytesRead: longint;
+        handleStream : THandleStream;
+        buff : array [0..BUFFER_SIZE-1] of byte;
+    begin
+        handleStream := THandleStream.create(getFileHandle(Output));
+        try
+            respBody.seek(0);
+            repeat
+                numBytesRead := respBody.read(buff, BUFFER_SIZE);
+                handleStream.write(buff, numBytesRead);
+            until (numBytesRead < BUFFER_SIZE);
+        finally
+            handleStream.free();
+        end;
+    end;
+
+    function TBinaryResponse.write() : IResponse;
+    begin
+        httpHeaders.setHeader('Content-Type', contentType);
+        httpHeaders.setHeader('Content-Length', intToStr(responseBody.size()));
+        httpHeaders.writeHeaders();
+        writeToStdOutput(responseBody);
+        result := self;
+    end;
+
+    function TBinaryResponse.clone() : ICloneable;
+    var clonedObj : IResponse;
+    begin
+        clonedObj := TBinaryResponse.create(
+            httpHeaders.clone() as IHeaders,
+            contentType,
+            //not clone response body as they may be big in size
+            responseBody
+        );
+        result := clonedObj;
+    end;
+
+    (*!------------------------------------
+     * get response body
+     *-------------------------------------
+     * @return response body
+     *-------------------------------------*)
+    function TBinaryResponse.body() : IResponseStream;
+    begin
+        result := responseBody;
+    end;
+end.
--- Http/Response/Contracts/ResponseStreamIntf.pas
+++ Http/Response/Contracts/ResponseStreamIntf.pas
@@ -25,6 +25,20 @@ type
         ['{14394487-875D-4C4E-B4AE-9176B7393CAC}']
 
         (*!------------------------------------
+         * read stream to buffer from current
+         * seek position
+         *-------------------------------------
+         * @param buffer pointer to buffer to store
+         * @param sizeToRead number of bytes to read
+         * @return number of bytes actually read
+         *-------------------------------------
+         * After read, implementation must advance its
+         * internal pointer position to actual number
+         * of bytes read
+         *-------------------------------------*)
+        function read(var buffer; const sizeToRead : longint) : longint;
+
+        (*!------------------------------------
          * write buffer to stream
          *-------------------------------------
          * @param buffer pointer to buffer to write
@@ -32,10 +46,26 @@ type
          * @return number of bytes actually written
          *-------------------------------------*)
         function write(const buffer; const sizeToWrite : longint) : longint;
+
+        (*!------------------------------------
+         * get stream size
+         *-------------------------------------
+         * @return size of stream in bytes
+         *-------------------------------------*)
+        function size() : longint;
+
+        (*!------------------------------------
+         * seek
+         *-------------------------------------
+         * @param offset in bytes to seek start from beginning
+         * @return actual offset
+         *-------------------------------------
+         * if offset >= stream size then it is capped
+         * to stream size-1
+         *-------------------------------------*)
+        function seek(const offset : longint) : longint;
     end;
 
 implementation
-
-
 
 end.
