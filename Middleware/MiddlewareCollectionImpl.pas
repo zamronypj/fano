1e30fe17c2677d14409a2528dc02e4108c726256
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-11-06 18:58:37 +0800

    edit license in comment

--- Middleware/MiddlewareCollectionImpl.pas
+++ Middleware/MiddlewareCollectionImpl.pas
@@ -3,7 +3,7 @@
  *
  * @link      https://github.com/fanoframework/fano
  * @copyright Copyright (c) 2018 Zamrony P. Juhara
- * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (GPL 3.0)
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
  *}
 
 unit MiddlewareCollectionImpl;
