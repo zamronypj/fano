bd2a63ea39daaf0131e912b6cb9d1deec2e861eb
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-11-16 17:41:02 +0800

    initial replace ITemplateFetcher with IViewPartial

---
+++ Mvc/Views/Contracts/PartialIntf.pas
@@ -0,0 +1,46 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit PartialIntf;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    ViewParametersIntf;
+
+type
+
+    (*!------------------------------------------------
+     * interface for any class having capability to
+     * read template file and replace its content with value
+     * and output to string
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    IPartial = interface
+        ['{220077B3-F4A1-4EEB-91FF-AB3E3A2E85F7}']
+
+        (*!------------------------------------------------
+         * read template file, parse and replace its variable
+         * and output to string
+         *-----------------------------------------------
+         * @param templatePath file path of template
+         * @param viewParams instance contains view parameters
+         *-----------------------------------------------*)
+        function fetch(
+            const templatePath : string;
+            const viewParams : IViewParameters
+        ) : string;
+    end;
+
+implementation
+end.
