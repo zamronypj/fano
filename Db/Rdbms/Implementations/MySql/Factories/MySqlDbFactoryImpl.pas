593284be4d5ba47d56d26fd3cd8bd1f1dabc663a
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-11-19 19:32:57 +0800

    add missing unit uses

---
+++ Db/Rdbms/Implementations/MySql/Factories/MySqlDbFactoryImpl.pas
@@ -0,0 +1,56 @@
+{*!
+ * Fano Web Framework (https://fano.juhara.id)
+ *
+ * @link      https://github.com/zamronypj/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/zamronypj/fano/blob/master/LICENSE (GPL 3.0)
+ *}
+
+unit MySqlDbFactoryImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    DependencyIntf,
+    DepenContainerIntf,
+    FactoryImpl;
+
+type
+
+    (*!------------------------------------------------
+     * basic class having capability to
+     * handle mysql relational database operation
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-------------------------------------------------*)
+    TMySQLDbFactory = class(TFactory)
+    public
+        (*!---------------------------------------------------
+         * build class instance
+         *----------------------------------------------------
+         * @param container dependency container instance
+         *---------------------------------------------------*)
+        function build(const container : IDependencyContainer) : IDependency; override;
+    end;
+
+implementation
+
+uses
+
+    MySqlDbImpl,
+
+    (*!---------------------------------------------------
+     * build class instance
+     *----------------------------------------------------
+     * @param container dependency container instance
+     *---------------------------------------------------*)
+    function TMySQLDbFactory.build(const container : IDependencyContainer) : IDependency; override;
+    begin
+        result := TMySQLDb.create();
+    end;
+
+end.
