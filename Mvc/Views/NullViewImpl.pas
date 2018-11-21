0544a578931c1287994b022fd314181a1db9c635
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-11-21 11:55:07 +0800

    implement null view

---
+++ Mvc/Views/NullViewImpl.pas
@@ -0,0 +1,61 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit NullViewImpl;
+
+interface
+
+uses
+
+    DependencyIntf,
+    ResponseIntf,
+    ViewIntf,
+    ViewParametersIntf,
+    InjectableObjectImpl;
+
+type
+
+    (*!-----------------------------------------------
+     * view that display nothing
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *------------------------------------------------*)
+    TNullView = class(TInjectableObject, IView)
+    public
+        (*!------------------------------------------------
+         * render view
+         *------------------------------------------------
+         * @param viewParams view parameters
+         * @param response response instance
+         * @return response
+         *-----------------------------------------------*)
+        function render(
+            const viewParams : IViewParameters;
+            const response : IResponse
+        ) : IResponse;
+    end;
+
+implementation
+
+    (*!------------------------------------------------
+     * render view
+     *------------------------------------------------
+     * @param viewParams view parameters
+     * @param response response instance
+     * @return response
+     *-----------------------------------------------*)
+    function TNullView.render(
+        const viewParams : IViewParameters;
+        const response : IResponse
+    ) : IResponse;
+    begin
+        //intentionally does nothing
+        result := response;
+    end;
+
+end.
