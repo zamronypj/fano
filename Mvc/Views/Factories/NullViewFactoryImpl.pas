fc77a976d3524524fcba5cce277fcf09d22ed909
Zamrony P. Juhara <zamronypj@yahoo.com>
2018-12-02 09:27:29 +0800

    implement null view factory

---
+++ Mvc/Views/Factories/NullViewFactoryImpl.pas
@@ -0,0 +1,45 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit NullViewFactoryImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    DependencyIntf,
+    DependencyContainerIntf,
+    FactoryImpl;
+
+type
+
+    (*!------------------------------------------------
+     * basic class that can create null view
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-------------------------------------------------*)
+    TNullViewFactory = class(TFactory)
+    public
+        function build(const container : IDependencyContainer) : IDependency; override;
+    end;
+
+implementation
+
+uses
+
+    NullViewImpl;
+
+    function TNullViewFactory.build(const container : IDependencyContainer) : IDependency;
+    begin
+        //create null view
+        result := TNullView.create();
+    end;
+end.
