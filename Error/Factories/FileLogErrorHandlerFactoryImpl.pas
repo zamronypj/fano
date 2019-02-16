3baecd8bf359e8f4f7177979edf5b5df876c37e6
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-02-16 10:59:14 +0800

    add missing factory

---
+++ Error/Factories/FileLogErrorHandlerFactoryImpl.pas
@@ -0,0 +1,47 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit LogErrorHandlerFactoryImpl;
+
+interface
+
+{$MODE OBJFPC}
+
+uses
+    DependencyIntf,
+    DependencyContainerIntf,
+    FactoryImpl;
+
+type
+
+    TFileLogErrorHandlerFactory = class(TFactory, IDependencyFactory)
+    private
+        logFilename : string;
+    public
+        constructor create(const filename : string);
+        function build(const container : IDependencyContainer) : IDependency; override;
+    end;
+
+implementation
+
+uses
+
+    FileLoggerImpl,
+    LogErrorHandlerImpl;
+
+    constructor TFileLogErrorHandlerFactory.create(const filename : string);
+    begin
+        logFilename := filename;
+    end;
+
+    function TFileLogErrorHandlerFactory.build(const container : IDependencyContainer) : IDependency;
+    begin
+        result := TLogErrorHandler.create(TFileLogger.create(logFilename));
+    end;
+
+end.
