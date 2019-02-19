a0e16c6cf61941ed20f30885426eca19cad19351
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-02-19 18:54:22 +0800

    simplify loading HTML template from file

---
+++ Mvc/Views/TemplateImpl.pas
@@ -0,0 +1,67 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit TemplateImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    ViewImpl,
+    TemplateParserIntf,
+    FileReaderIntf;
+
+type
+
+    (*!------------------------------------------------
+     * View that can render from a HTML template file
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-------------------------------------------------*)
+    TTemplate = class(TView)
+    public
+
+        (*!------------------------------------------------
+         * constructor
+         *------------------------------------------------
+         * @param tplPath template filepath
+         * @param templateParserInst template variable parser
+         * @param templateReaderInst template file reader
+         *-----------------------------------------------*)
+        constructor create(
+            const tplPath : string;
+            const templateParserInst : ITemplateParser;
+            const templateReaderInst : IFileReader
+        );
+    end;
+
+implementation
+
+    (*!------------------------------------------------
+     * constructor
+     *------------------------------------------------
+     * @param tplPath template filepath
+     * @param templateParserInst template variable parser
+     * @param templateReaderInst template file reader
+     *-----------------------------------------------*)
+    constructor TTemplate.create(
+        const tplPath : string;
+        const templateParserInst : ITemplateParser;
+        const templateReaderInst : IFileReader
+    );
+    begin
+        inherited create(
+            templateParserInst,
+            templateReaderInst.readFile(tplPath)
+        );
+    end;
+
+end.
