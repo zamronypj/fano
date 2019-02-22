dc6cf94b0044014bc3ae649bb557bec0901ee528
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-02-22 19:45:42 +0800

    add missing unit

---
+++ Libs/KeyValues/Factories/KeyValuePairFactoryImpl.pas
@@ -0,0 +1,54 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit KeyValuePairFactoryImpl;
+
+interface
+
+{$MODE OBJFPC}
+
+uses
+
+    DependencyIntf,
+    DependencyContainerIntf,
+    FactoryImpl;
+
+type
+    (*!------------------------------------------------
+     * TKeyValuePair factory class
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    TKeyValuePairFactory = class(TFactory)
+    public
+        (*!---------------------------------------------------
+         * build class instance
+         *----------------------------------------------------
+         * @param container dependency container instance
+         *----------------------------------------------------
+         * This is implementation of IDependencyFactory
+         *---------------------------------------------------*)
+        function build(const container : IDependencyContainer) : IDependency; override;
+    end;
+
+implementation
+
+uses
+
+    KeyValuePairImpl;
+
+    (*!---------------------------------------------------
+     * build class instance
+     *----------------------------------------------------
+     * @param container dependency container instance
+     *---------------------------------------------------*)
+    function TKeyValuePairFactory.build(const container : IDependencyContainer) : IDependency;
+    begin
+        result := TKeyValuePairFactory.create();
+    end;
+end.
