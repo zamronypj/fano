f4829476ed99cdf7279f2db05975079335df55b1
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-05-13 12:22:11 +0800

    edit code comment

--- Libs/HttpClient/Implementations/Curl/HttpMethodImpl.pas
+++ Libs/HttpClient/Implementations/Curl/HttpMethodImpl.pas
@@ -144,7 +144,7 @@ resourcestring
     constructor THttpMethod.create(const fStream : IResponseStream);
     begin
         //libcurl only knows raw pointer, so we use raw pointer to hold
-        //instance of IStreamAdapter interface. But because typecast interface
+        //instance of IResponseStream interface. But because typecast interface
         //to pointer does not do automatic reference counting,
         //we must add reference count manually by calling _AddRef() method
         pStream := pointer(fStream);
@@ -163,7 +163,7 @@ resourcestring
         inherited destroy();
 
         //libcurl only knows raw pointer, so we use raw pointer to hold
-        //instance of IStreamAdapter interface. But because typecast interface
+        //instance of IResponseStream interface. But because typecast interface
         //to pointer does not do automatic reference counting,
         //we must decrement reference count manually by calling _Release() method
         IResponseStream(pStream)._release();
