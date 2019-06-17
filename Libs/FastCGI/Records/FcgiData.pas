4a18b7d7bdbe49588f5596fb557c3e4dc0f5c41f
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-06-17 20:42:19 +0800

    fix wrong parent class

--- Libs/FastCGI/Records/FcgiData.pas
+++ Libs/FastCGI/Records/FcgiData.pas
@@ -16,7 +16,7 @@ interface
 uses
 
     StreamAdapterIntf,
-    FcgiStreamRecord;
+    FcgiRecord;
 
 type
 
@@ -25,7 +25,7 @@ type
      *
      * @author Zamrony P. Juhara <zamronypj@yahoo.com>
      *-----------------------------------------------*)
-    TFcgiData = class(TFcgiStreamRecord)
+    TFcgiData = class(TFcgiRecord)
     public
         constructor create(const dataStream : IStreamAdapter; const requestId : word);
     end;
