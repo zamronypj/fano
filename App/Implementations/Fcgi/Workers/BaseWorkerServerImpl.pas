85d355b200fb8df8008a7616f732b081b80ee062
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-06-17 20:23:06 +0800

    add missing files

---
+++ App/Implementations/Fcgi/Workers/BaseWorkerServerImpl.pas
@@ -0,0 +1,91 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+unit BaseWorkerServerImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+
+uses
+    Classes,
+    SysUtils,
+    Sockets,
+    SSockets,
+    DataAvailListenerIntf,
+    RunnableWithDataNotifIntf;
+
+type
+
+    (*!-----------------------------------------------
+     * Base FastCGI web application worker server implementation
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    TBaseWorkerServer = class(TInjectableObject, IRunnableWithDataNotif)
+    protected
+        fDataListener : IDataAvailListener;
+        fServer : TSocketServer;
+
+        procedure DoConnect(Sender: TObject; Data: TSocketStream);
+    public
+        constructor create();
+        destructor destroy(); override;
+
+        (*!------------------------------------------------
+        * set instance of class that will be notified when
+        * data is available
+        *-----------------------------------------------
+        * @param dataListener, class that wish to be notified
+        * @return true current instance
+        *-----------------------------------------------*)
+        function setDataAvailListener(const dataListener : IDataAvailListener) : IRunnableWithDataNotif;
+    end;
+
+implementation
+
+    constructor TBaseWorkerServer.create();
+    begin
+        fDataListener := nil;
+        fServer := nil;
+    end;
+
+    destructor TBaseWorkerServer.destroy();
+    begin
+        inherited destroy();
+        freeAndNil(fServer);
+        fDataListener := nil;
+    end;
+
+    (*!------------------------------------------------
+    * set instance of class that will be notified when
+    * data is available
+    *-----------------------------------------------
+    * @param dataListener, class that wish to be notified
+    * @return true current instance
+    *-----------------------------------------------*)
+    function TBaseWorkerServer.setDataAvailListener(const dataListener : IDataAvailListener) : IRunnableWithDataNotif;
+    begin
+        fDataListener := dataListener;
+    end;
+
+    procedure TBaseWorkerServer.DoConnect(Sender: TObject; Data: TSocketStream);
+    begin
+        if (assigned(fDataListener)) then
+        begin
+            fDataListener.handleData(TStreamAdapter.create(data), sender);
+        end;
+    end;
+
+    function TBaseWorkerServer.run() : IRunnable;
+    begin
+        fServer.startAccepting();
+        result := self;
+    end;
+end.
