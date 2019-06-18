9a7ca889f2fb444ff32eb810d27f2196ef061246
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-06-18 11:49:22 +0800

    fix add missing file

---
+++ Libs/FastCGI/Request/Contracts/RequestIdAwareIntf.pas
@@ -0,0 +1,37 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit RequestIdAwareIntf;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+type
+
+    (*!-----------------------------------------------
+     * Interface for any class having capability to
+     * return request id
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    IRequestIdAware = interface
+        ['{9718C418-55F4-4435-B575-965389D836A4}']
+
+        (*!------------------------------------------------
+         * get request id
+         *-----------------------------------------------
+         * @return request id
+         *-----------------------------------------------*)
+        function getRequestId() : word;
+    end;
+
+implementation
+
+end.
