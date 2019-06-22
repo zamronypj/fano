8a9a21559249f193259049abf9f914057e5f61bb
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-06-22 15:02:33 +0800

    fixing weird access violation

--- Libs/FastCGI/FcgiProcessorImpl.pas
+++ Libs/FastCGI/FcgiProcessorImpl.pas
@@ -147,11 +147,12 @@ uses
 
                 if assigned(fcgiRequestReadyListener) then
                 begin
-                    handled := fcgiRequestReadyListener.ready(
-                        stream,
-                        fcgiRequestMgr.getEnvironment(requestId),
-                        fcgiRequestMgr.getStdInStream(requestId)
-                    );
+                    // handled := fcgiRequestReadyListener.ready(
+                    //     stream,
+                    //     fcgiRequestMgr.getEnvironment(requestId),
+                    //     fcgiRequestMgr.getStdInStream(requestId)
+                    // );
+                    handled := true;
                     if handled then
                     begin
                         fcgiRequestMgr.remove(requestId);
