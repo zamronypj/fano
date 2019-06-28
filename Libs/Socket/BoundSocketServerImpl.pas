e56759cfa0f216f4919471abbf783b07004fd265
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-06-28 21:49:19 +0800

    implement abstract method

--- Libs/Socket/BoundSocketServerImpl.pas
+++ Libs/Socket/BoundSocketServerImpl.pas
@@ -30,12 +30,18 @@ type
     TBoundSocketServer = class(TSocketServer)
     protected
         procedure bind(); override;
+        function accept(): longint; override;
+        function getConnection() : TSocketStream; override;
     public
         procedure listen();
     end;
 
 implementation
 
+resourcestring
+
+    strSocketAcceptFailed = 'Could not accept a client connection on socket: %d, error %d';
+    strSocketAcceptWouldBlock = 'Accept would block on socket: %d';
 
     procedure TBoundSocketServer.bind();
     begin
@@ -44,8 +50,47 @@ implementation
         fBound := true;
     end;
 
+    function TBoundSocketServer.accept(): longint;
+    begin
+        result := sockets.fpAccept(socket, nil, nil);
+        If result < 0 then
+        begin
+            if SocketError = ESysEWOULDBLOCK then
+            begin
+                raise ESocketError.Create(seAcceptWouldBlock,[socket]);
+            end else
+            begin
+                raise ESocketError.Create(seAcceptFailed,[ socket, socketError ]);
+            end
+        end;
+    end;
+
     procedure TBoundSocketServer.listen();
     begin
         //socket is assumed already bound and listening, so do nothing here
+    end;
+
+    function TBoundSocketServer.getConnection() : TSocketStream;
+    var newSocket : longint;
+    begin
+        result := nil;
+        newSocket := accept();
+        if (newSocket < 0) then
+        begin
+            raise ESocketError.Create(seAcceptFailed,[Socket,SocketError]);
+        end;
+
+        if FAccepting and DoConnectQuery(newSocket) then
+        begin
+            result := SockToStream(newSocket);
+        end else
+        begin
+            closeSocket(newSocket);
+        end;
+    end;
+
+    function TBoundSocketServer.SockToStream(aSocket : longint) : TSocketStream;
+    begin
+        result := TSocketStream.create(aSocket);
     end;
 end.
