d82ddf6f44d5875fa95c8ff5be76b89d72f5b78d
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-07-19 14:15:16 +0800

    initial implementation

---
+++ Libs/SCGI/ScgiProcessorImpl.pas
@@ -0,0 +1,79 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit ScgiProcessorImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    StreamAdapterIntf,
+    CloseableIntf,
+    ReadyListenerIntf;
+
+type
+
+    (*!-----------------------------------------------
+     * Class which can process SCGI stream from web server
+     *
+     * @link https://python.ca/scgi/protocol.txt
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    TScgiProcessor = class(TInterfacedObject, IProtocolProcessor)
+    public
+
+        (*!------------------------------------------------
+         * process request stream
+         *-----------------------------------------------*)
+        procedure process(const stream : IStreamAdapter; const streamCloser : ICloseable);
+
+        (*!------------------------------------------------
+         * get StdIn stream for complete request
+         *-----------------------------------------------*)
+        function getStdIn() : IStreamAdapter;
+
+        (*!------------------------------------------------
+         * set listener to be notified when request is ready
+         *-----------------------------------------------
+         * @return current instance
+         *-----------------------------------------------*)
+        function setReadyListener(const listener : IReadyListener) : IProtocolProcessor;
+    end;
+
+implementation
+
+    (*!------------------------------------------------
+     * process request stream
+     *-----------------------------------------------*)
+    procedure TScgiProcessor.process(const stream : IStreamAdapter; const streamCloser : ICloseable);
+    begin
+
+    end;
+
+    (*!------------------------------------------------
+     * get StdIn stream for complete request
+     *-----------------------------------------------*)
+    function TScgiProcessor.getStdIn() : IStreamAdapter;
+    begin
+
+    end;
+
+    (*!------------------------------------------------
+     * set listener to be notified weh request is ready
+     *-----------------------------------------------
+     * @return current instance
+     *-----------------------------------------------*)
+    function TScgiProcessor.setReadyListener(const listener : IReadyListener) : IProtocolProcessor;
+    begin
+
+    end;
+
+end.
