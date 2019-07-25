6f0ed97c926e2308ed36dab940724eefe2c6a943
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-07-25 08:37:32 +0800

    add missing file

---
+++ src/fano.pas
@@ -0,0 +1,38 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit fano;
+
+interface
+
+{$MODE OBJFPC}
+
+uses
+
+    {$INCLUDE Includes/interfaces.inc}
+    {$INCLUDE Includes/implementations.inc}
+    {$INCLUDE Includes/types.inc}
+
+type
+    (*!-----------------------------------------------
+     * Redeclare all classes in one unit to simplify
+     * using classes in application code
+     *-------------------------------------------------
+     * If you use this unit, then you do not need to include
+     * other unit otherwise you will get compilation error
+     * about duplicate identifier
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *------------------------------------------------*)
+
+    {$INCLUDE Includes/interfaces.aliases.inc}
+    {$INCLUDE Includes/implementations.aliases.inc}
+
+implementation
+
+end.
