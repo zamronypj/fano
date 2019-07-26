5a3f32e32f1214c3c21823aa28d8ceeb72cf6ced
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-07-27 06:00:31 +0800

    implement shell script to clean compiled unit binaries

--- tests/clean.sh
+++ tests/clean.sh
@@ -8,23 +8,8 @@
 #!/bin/bash
 
 #------------------------------------------------------
-# Build script for Linux
+# Scripts to delete all compiled unit files
 #------------------------------------------------------
 
-if [[ -z "${FANO_DIR}" ]]; then
-export FANO_DIR="../"
-fi
-
-if [[ -z "${UNIT_OUTPUT_DIR}" ]]; then
-    export UNIT_OUTPUT_DIR="bin/unit"
-fi
-
-if [[ -z "${EXEC_OUTPUT_DIR}" ]]; then
-    export EXEC_OUTPUT_DIR="bin"
-fi
-
-if [[ -z "${EXEC_OUTPUT_NAME}" ]]; then
-    export EXEC_OUTPUT_NAME="testrunner"
-fi
-
-fpc @testrunner.cfg testrunner.pas
+find bin/unit ! -name 'README.md' -type f -exec rm -f {} +
+find bin ! -name 'README.md' -type f -exec rm -f {} +
