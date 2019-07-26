7c97bfbeaf46b371e2c525d6a505f8beba0d45ff
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-07-26 08:35:09 +0800

    change tmp variable type

--- src/Libs/Socket/SocketSvrImpl.pas
+++ src/Libs/Socket/SocketSvrImpl.pas
@@ -255,7 +255,7 @@ var
      * @param listenSocket, listen socket handle
      *-----------------------------------------------*)
     procedure TSocketSvr.makeNonBlockingSocket(listenSocket : longint);
-    var flags : integer;
+    var flags : longint;
     begin
         //read control flag and set listen socket to be non blocking
         flags := fpFcntl(listenSocket, F_GETFL, 0);
