97033d69a2b4b7100daa0820576649fdf9a429e2
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-07-27 05:23:46 +0800

    initial implementation of test runner

---
+++ tests/testrunner.pas
@@ -0,0 +1,37 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+program testrunner;
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    classes,
+    consoletestrunner,
+    fano;
+
+type
+
+    TFanoTestRunner = class(TTestRunner)
+    end;
+
+var
+
+  fanoTestRunner: TFanoTestRunner;
+
+begin
+    DefaultFormat := fPlain;
+    DefaultRunAllTests := true;
+    fanoTestRunner := TFanoTestRunner.Create(nil);
+    fanoTestRunner.initialize();
+    fanoTestRunner.title := 'Fano Test Runner';
+    fanoTestRunner.run();
+    fanoTestRunner.free();
+end.
