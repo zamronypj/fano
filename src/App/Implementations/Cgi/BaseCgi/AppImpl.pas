84a7fedece6e8f890b01eb2e5e7dd12f1b8d98bd
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-08-28 07:55:19 +0800

    remove missing variable

--- src/App/Implementations/Cgi/BaseCgi/AppImpl.pas
+++ src/App/Implementations/Cgi/BaseCgi/AppImpl.pas
@@ -60,19 +60,19 @@ uses
         except
             on e : ERouteHandlerNotFound do
             begin
-                errorHandler.handleError(envEnum, e, 404, sHttp404Message);
+                errorHandler.handleError(environment.enumerator, e, 404, sHttp404Message);
                 reset();
             end;
 
             on e : EMethodNotAllowed do
             begin
-                errorHandler.handleError(envEnum, e, 405, sHttp405Message);
+                errorHandler.handleError(environment.enumerator, e, 405, sHttp405Message);
                 reset();
             end;
 
             on e : Exception do
             begin
-                errorHandler.handleError(envEnum, e);
+                errorHandler.handleError(environment.enumerator, e);
                 reset();
             end;
         end;
