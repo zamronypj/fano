06d4750760835eb64faa5496d75935c5462f588b
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-08-28 19:06:08 +0800

    add missing unit

--- src/Libs/IO/StandardInput/SimpleStdInReaderImpl.pas
+++ src/Libs/IO/StandardInput/SimpleStdInReaderImpl.pas
@@ -17,6 +17,7 @@ uses
 
     sysutils,
     InjectableObjectImpl,
+    StreamAdapterIntf,
     StdInIntf;
 
 type
--- src/Libs/IO/StandardInput/StdInFromStringImpl.pas
+++ src/Libs/IO/StandardInput/StdInFromStringImpl.pas
@@ -17,6 +17,7 @@ uses
 
     SysUtils,
     InjectableObjectImpl,
+    StreamAdapterIntf,
     StdInIntf;
 
 type
