313d8abb895f6e1178d0333631d995b61bd55f60
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-09-25 11:44:47 +0800

    implement middleware list factory

---
+++ src/Middleware/Factories/MiddlewareListFactoryImpl.pas
@@ -0,0 +1,43 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit MiddlewareListFactoryImpl;
+
+interface
+
+{$MODE OBJFPC}
+
+uses
+
+    MiddlewareListFactoryIntf,
+    MiddlewareListItemFactoryIntf;
+
+type
+
+    (*!------------------------------------------------
+     * basic class having capability to create
+     * middleware collection aware instance
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-------------------------------------------------*)
+    TMiddlewareListFactory = class(TInterfacedObject IMiddlewareListFactory)
+    public
+        function build() : IMiddlewareListItem;
+    end;
+
+implementation
+
+uses
+
+    MiddlewareListImpl;
+
+    function TMiddlewareListFactory.build() : IMiddlewareListItem;
+    begin
+        result := TMiddlewareList.create();
+    end;
+end.
