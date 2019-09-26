c9ec4e77a6e18324e90d62719359d5a7bd149192
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-09-26 12:08:05 +0800

    fix wrong parameter

--- src/Config/Implementations/Ini/Factories/IniFileConfigFactoryImpl.pas
+++ src/Config/Implementations/Ini/Factories/IniFileConfigFactoryImpl.pas
@@ -18,8 +18,10 @@ type
     TIniFileConfigFactory = class (TFactory, IDependencyFactory)
     private
         configFilename : string;
+        fDefaultSection : string;
     public
         constructor create(const configFile :string);
+        function setDefaultSection(const defaultSection : string) : TIniFileConfigFactory;
         function build(const container : IDependencyContainer) : IDependency; override;
     end;
 
@@ -32,11 +34,18 @@ uses
     constructor TIniFileConfigFactory.create(const configFile : string);
     begin
         configFilename := configFile;
+        fDefaultSection := 'fano';
+    end;
+
+    function TIniFileConfigFactory.setDefaultSection(const defaultSection : string) : TIniFileConfigFactory;
+    begin
+        fDefaultSection := defaultSection;
+        result := self;
     end;
 
     function TIniFileConfigFactory.build(const container : IDependencyContainer) : IDependency;
     begin
-        result := TIniFileConfig.create(configFilename);
+        result := TIniFileConfig.create(configFilename, fDefaultSection);
     end;
 
 end.
