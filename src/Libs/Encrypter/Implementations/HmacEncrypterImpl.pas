af2371d64b240f5054e179f0f5dbb04dbc0812d7
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-11-25 11:27:37 +0800

    add implementation of encrypter decrypter which support data integrity check

---
+++ src/Libs/Encrypter/Implementations/HmacEncrypterImpl.pas
@@ -0,0 +1,145 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit HmacEncrypterImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    InjectableObjectImpl,
+    EncrypterIntf,
+    DecrypterIntf;
+
+type
+
+    (*!------------------------------------------------
+     * abstract class having capability to encrypt/decrypt string
+     * encoded as [hmac][separator][ciphertext]
+     * hmac will be used to check for ciphertext data integrity
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    THmacEncrypter = class(TInjectableObject, IEncrypter, IDecrypter)
+    private
+        fEncrypter : IEncrypter;
+        fDecrypter : IDecrypter;
+        fSecretKey : string;
+        fSeparator : string;
+    protected
+        function calcHmac(const secretKey : string; const msg : string) : string; virtual; abstract;
+    public
+        constructor create(
+            const encrypter : IEncrypter;
+            const decrypter : IDecrypter;
+            const secretKey : string;
+            const separator : string = '||'
+        );
+        destructor destroy(); override;
+
+        (*!------------------------------------------------
+         * encrypt string as [hmac][separator][ciphertext]
+         *-----------------------------------------------
+         * @param originalStr original string
+         * @return encrypted string
+         *-----------------------------------------------*)
+        function encrypt(const originalStr : string) : string;
+
+        (*!------------------------------------------------
+         * decrypt string
+         *-----------------------------------------------
+         * @param encryptedStr encrypted string
+         * @return original string
+         * @throw EReadError exception when ciphertext is tampered
+         *-----------------------------------------------*)
+        function decrypt(const encryptedStr : string) : string;
+
+    end;
+
+implementation
+
+uses
+
+    Classes,
+    SysUtils,
+    hmac;
+
+resourcestring
+
+    sErrorDecrypt = 'Error decrypting text';
+
+    constructor THmacEncrypter.create(
+        const encrypter : IEncrypter;
+        const decrypter : IDecrypter;
+        const secretKey : string;
+        const separator : string = '||'
+    );
+    begin
+        fEncrypter := encrypter;
+        fDecrypter := decrypters;
+        fSecretKey := secretKey;
+        fSeparator := separator;
+    end;
+
+    destructor THmacEncrypter.destroy();
+    begin
+        fEncrypter := nil;
+        fDecrypter := nil;
+        inherited destroy();
+    end;
+
+    (*!------------------------------------------------
+     * encrypt string as [hmac][separator][ciphertext]
+     *-----------------------------------------------
+     * @param originalStr original string
+     * @return encrypted string
+     *-----------------------------------------------*)
+    function THmacEncrypter.encrypt(const originalStr : string) : string;
+    var ciphertext : string;
+    begin
+        ciphertext := fEncrypter.encrypt(originalStr);
+        result := calcHmac(fSecretKey, ciphertext) + fSeparator + ciphertext;
+    end;
+
+    (*!------------------------------------------------
+     * decrypt string
+     *-----------------------------------------------
+     * @param encryptedStr encrypted string with format [hmac][separator][ciphertext]
+     * @return original string
+     * @throw EReadError exception when ciphertext is tampered
+     *-----------------------------------------------*)
+    function THmacEncrypter.decrypt(const encryptedStr : string) : string;
+    var separatorPos : integer;
+        hmac : string;
+        ciphertext : string;
+    begin
+        separatorPos := pos(fSeparator, encryptedStr);
+        if (separatorPos = 0) then
+        begin
+            //cipher text may be tampered
+            raise EReadError.create(sErrorDecrypt);
+        end;
+
+        //extract hmac and ciphertext from encryptedStr
+        hmac := copy(encryptedStr, 1, separatorPos - 1);
+        ciphertext := copy(encryptedStr, separatorPos + 2, length(encryptedStr) - separatorPos - 1);
+
+        if (hmac = calcHmac(fSecretKey, ciphertext)) then
+        begin
+            //ciphertext is not tampered, continue to decrypt it
+            result := fDecrypter.decrypt(ciphertext);
+        end else
+        begin
+            //cipher text may be tampered
+            raise EReadError.create(sErrorDecrypt);
+        end;
+    end;
+end.
---
+++ src/Libs/Encrypter/Implementations/Md5HmacEncrypterImpl.pas
@@ -0,0 +1,45 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit Md5HmacEncrypterImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    HmacEncrypterImpl;
+
+type
+
+    (*!------------------------------------------------
+     * class having capability to encrypt/decrypt string
+     * encoded as [hmac][separator][ciphertext]
+     * hmac MD5 will be used to check for ciphertext data integrity
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    TMd5HmacEncrypter = class(THmacEncrypter)
+    protected
+        function calcHmac(const secretKey : string; const msg : string) : string; override;
+    end;
+
+implementation
+
+uses
+
+    hmac;
+
+    function TMD5HmacEncrypter.calcHmac(const secretKey : string; const msg : string) : string;
+    begin
+        result := HMACMD5Print(HMACMD5Digest(secretKey, msg));
+    end;
+
+end.
