ce27e17db666d3470a340ab89e8a99455f8de91c
Zamrony P. Juhara <zamronypj@yahoo.com>
2019-12-12 09:50:15 +0800

    implement session id generator wich add IP addres and current time

---
+++ src/Sessions/Implementations/IdGenerators/Factories/IpKeyGuidSessionIdGeneratorFactoryImpl.pas
@@ -0,0 +1,74 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit IpKeyGuidSessionIdGeneratorFactoryImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    SessionIdGeneratorIntf,
+    SessionIdGeneratorFactoryIntf;
+
+type
+
+    (*!------------------------------------------------
+     * class having capability to
+     * create session id generator which use SHA1 of
+     * IP address + time + secret key + guid
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    TIpKeyGuidSessionIdGeneratorFactory = class(TInterfacedObject, ISessionIdGeneratorFactory)
+    private
+        fSecretKey : string;
+    public
+        constructor create(const secretKey : string);
+
+        (*!------------------------------------
+         * build session id generator instance
+         *-------------------------------------
+         * @return session id generator instance
+         *-------------------------------------*)
+        function build() : ISessionIdGenerator;
+    end;
+
+implementation
+
+uses
+
+    IpTimeSessionIdGeneratorImpl,
+    KeySessionIdGeneratorImpl,
+    GuidSessionIdGeneratorImpl,
+    Sha1SessionIdGeneratorImpl;
+
+    constructor TIpKeyGuidSessionIdGeneratorFactory.create(const secretKey : string);
+    begin
+        fSecretKey := secretKey;
+    end;
+
+    (*!------------------------------------
+     * build session id generator instance
+     *-------------------------------------
+     * @return session id generator instance
+     *-------------------------------------*)
+    function TIpKeyGuidSessionIdGeneratorFactory.build() : ISessionIdGenerator;
+    begin
+        result := TSha1SessionIdGenerator.create(
+            TIpTimeSessionIdGenerator.create(
+                TKeySessionIdGenerator.create(
+                    TGuidSessionIdGenerator.create(),
+                    fSecretKey
+                )
+            )
+        );
+    end;
+end.
