32fab3a5346bdb457059eaeeaff35824a0451b6c
Zamrony P. Juhara <zamronypj@yahoo.com>
2020-01-26 16:29:39 +0800

    initial implementation of refactored socket server

---
+++ src/Libs/Socket2/Implementations/SocketOptsImpl.pas
@@ -0,0 +1,51 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit SocketOptsImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+type
+
+    (*!------------------------------------------------
+     * class having capability setup
+     * socket options
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    TSocketOpts = class(TInterfacedObject, ISocketOpts)
+    public
+
+        (*!-----------------------------------------------
+        * make socket non blocking
+        *-------------------------------------------------
+        * @param listenSocket, listen socket handle
+        *-----------------------------------------------*)
+        procedure makeNonBlockingSocket(aSocket : longint);
+
+    end;
+
+implementation
+
+    (*!-----------------------------------------------
+     * make file descriptor/socket non blocking
+     *-------------------------------------------------
+     * @param fd, file descriptor or listen socket handle
+     *-----------------------------------------------*)
+    procedure TSocketOpts.makeNonBlockingSocket(fd : longint);
+    var flags : longint;
+    begin
+        //read control flag and set socket to be non blocking
+        flags := fpFcntl(fd, F_GETFL, 0);
+        fpFcntl(aSocket, F_SETFl, flags or O_NONBLOCK);
+    end;
+
+end.
---
+++ src/Libs/Socket2/Implementations/UnixSocketImpl.pas
@@ -0,0 +1,135 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit UnixSocketImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    Sockets,
+    AbstractSocketImpl;
+
+type
+
+    (*!------------------------------------------------
+     * class having capability setup Unix socket
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    TUnixSocket = class (TAbstractSocket)
+    private
+        fSocketFile : string;
+        FUnixAddr : TUnixSockAddr;
+        fSocketAddrLen : TSockLen;
+    protected
+        function createSocket() : longint; override;
+    public
+        (*!-----------------------------------------------
+         * constructor
+         *-------------------------------------------------
+         * @param filename socket filename
+         *-----------------------------------------------*)
+        constructor create(const filename : string);
+
+        (*!-----------------------------------------------
+         * return textual information regarding socket
+         *-----------------------------------------------*)
+        function getInfo() : string; override;
+
+        (*!-----------------------------------------------
+         * bind socket to socket address
+         *-----------------------------------------------*)
+        procedure bind(); override;
+
+        (*!-----------------------------------------------
+        * accept connection
+        *-------------------------------------------------
+        * @param listenSocket, socket handle
+        * @return client socket which data can be read
+        *-----------------------------------------------*)
+        function accept(listenSocket : longint) : longint; override;
+    end;
+
+implementation
+
+uses
+
+    BaseUnix,
+    Unix;
+
+    function TUnixSocket.createSocket() : longint;
+    begin
+        result := fpSocket(AF_UNIX, SOCK_STREAM, 0);
+    end;
+
+    function TUnixSocket.getInfo() : string;
+    begin
+        result := fSocketFile;
+    end;
+
+    (*!-----------------------------------------------
+     * constructor
+     *-------------------------------------------------
+     * @param filename socket filename
+     *-----------------------------------------------*)
+    constructor TUnixSocket.create(const filename : string);
+    begin
+        fSocketFile := filename;
+        inherited create();
+    end;
+
+    (*!-----------------------------------------------
+     * replacement for str2UnixSockAddr() which marked
+     * as deprecated
+     *-----------------------------------------------*)
+    procedure StrToUnixAddr(const addr : string; var sockAddr : TUnixSockAddr; var len:longint);
+    begin
+        move(addr[1], sockAddr.Path, length(addr));
+        sockAddr.Family := AF_UNIX;
+        sockAddr.Path[length(addr)] := #0;
+        len := length(addr) + 3;
+    end;
+
+    (*!-----------------------------------------------
+     * bind socket to socket address
+     *-----------------------------------------------*)
+    procedure TUnixSocket.bind();
+    var errCode : longint;
+        addrLen  : longint;
+    begin
+        StrToUnixAddr(fSocketFile, FUnixAddr, addrLen);
+        if fpBind(getSocket(), @FUnixAddr, addrLen) <> 0 then
+        begin
+            errCode := socketError();
+            raise ESockBind.createFmt(
+                rsBindFailed,
+                [ FSocketFile, strError(errCode), errCode ]
+            );
+        end;
+        fSocketAddrLen := addrLen;
+    end;
+
+    (*!-----------------------------------------------
+     * accept connection
+     *-------------------------------------------------
+     * @param listenSocket, socket handle
+     * @return client socket which data can be read
+     *-----------------------------------------------*)
+    function TUnixSocket.accept(listenSocket : longint) : longint; override;
+    var addrLen : TSockLen;
+    begin
+        addrLen := fSocketAddrLen;
+        result := fpAccept(listenSocket, @FUnixAddr, @addrLen);
+        fSocketAddrLen := addrLen;
+    end;
+
+end.
