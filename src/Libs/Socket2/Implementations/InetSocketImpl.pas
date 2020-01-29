9dfe8faa79eece1b1d6f544b4b5578ed90b196bf
Zamrony P. Juhara <zamronypj@yahoo.com>
2020-01-29 19:24:44 +0800

    fix syntax error

--- src/Libs/Socket2/Implementations/InetSocketImpl.pas
+++ src/Libs/Socket2/Implementations/InetSocketImpl.pas
@@ -101,7 +101,7 @@ uses
     (*!-----------------------------------------------
      * bind socket to an socket address
      *-----------------------------------------------*)
-    function TInetSocket.doBind() : longint; override;
+    function TInetSocket.doBind() : longint;
     begin
         FInetAddr.sin_family := AF_INET;
         FInetAddr.sin_port := htons(FPort);
@@ -115,7 +115,7 @@ uses
      * @param listenSocket, socket handle
      * @return client socket which data can be read
      *-----------------------------------------------*)
-    function TInetSocket.accept(listenSocket : longint) : longint; override;
+    function TInetSocket.accept(listenSocket : longint) : longint;
     var addrLen : TSockLen;
     begin
         addrLen := sizeof(FInetAddr);
