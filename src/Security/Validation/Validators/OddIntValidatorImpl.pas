854b62501624da8d7576044d05bf638c5522abab
Zamrony P. Juhara <zamronypj@yahoo.com>
2020-02-18 18:21:07 +0800

    add new type of integer validation rule

---
+++ src/Security/Validation/Validators/OddIntValidatorImpl.pas
@@ -0,0 +1,85 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit OddIntValidatorImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    ReadOnlyListIntf,
+    ValidatorIntf,
+    RequestIntf,
+    BaseValidatorImpl;
+
+type
+
+    (*!------------------------------------------------
+     * basic class having capability to
+     * validate integer data that must be odd value
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-------------------------------------------------*)
+    TOddIntValidator = class(TBaseValidator)
+    protected
+        (*!------------------------------------------------
+         * actual data validation
+         *-------------------------------------------------
+         * @param dataToValidate input data
+         * @return true if data is valid otherwise false
+         *-------------------------------------------------*)
+        function isValidData(
+            const dataToValidate : string;
+            const dataCollection : IReadOnlyList;
+            const request : IRequest
+        ) : boolean; override;
+    public
+        (*!------------------------------------------------
+         * constructor
+         *-------------------------------------------------*)
+        constructor create();
+    end;
+
+implementation
+
+uses
+
+    SysUtils;
+
+resourcestring
+
+    sErrFieldMustBeOddInteger = 'Field %s must be odd integer value';
+
+    (*!------------------------------------------------
+     * constructor
+     *-------------------------------------------------*)
+    constructor TOddIntValidator.create();
+    begin
+        inherited create(sErrFieldMustBeOddInteger);
+    end;
+
+    (*!------------------------------------------------
+     * actual data validation
+     *-------------------------------------------------
+     * @param dataToValidate input data
+     * @return true if data is valid otherwise false
+     *-------------------------------------------------*)
+    function TOddIntValidator.isValidData(
+        const dataToValidate : string;
+        const dataCollection : IReadOnlyList;
+        const request : IRequest
+    ) : boolean;
+    var actualVal : integer;
+    begin
+        result := tryStrToInt(dataToValidate, actualVal) and
+            ((actualVal mod 2) = 1);
+    end;
+end.
