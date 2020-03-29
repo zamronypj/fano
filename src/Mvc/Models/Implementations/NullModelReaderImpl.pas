aafa88277bf5ddf60d54c1a3d90a339fb899a8dd
Zamrony P. Juhara <zamronypj@yahoo.com>
2020-03-30 06:19:34 +0800

    implement null model reader

---
+++ src/Mvc/Models/Implementations/NullModelReaderImpl.pas
@@ -0,0 +1,92 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit NullModelReaderImpl;
+
+interface
+
+{$MODE OBJFPC}
+
+uses
+
+    ModelParamsIntf,
+    ModelResultSetIntf,
+    ModelReaderIntf,
+    InjectableObjectImpl;
+
+type
+
+    (*!------------------------------------------------
+     * null model reader implementation
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    TNullModelReader = class(TInjectableObject, IModelReader)
+    private
+        fNullResultSet : IModelResultSet;
+    public
+        constructor create();
+        destructor destroy(); override;
+
+        (*!----------------------------------------------
+         * read data from storage
+         *-----------------------------------------------
+         * @param params parameter for search/filtering
+         * @return model data
+         *-----------------------------------------------*)
+        function read(const params : IModelParams = nil) : IModelResultSet;
+
+        (*!----------------------------------------------
+         * return data instance after read() is execute
+         *-----------------------------------------------
+         * @return model data
+         *-----------------------------------------------*)
+        function data() : IModelResultSet;
+    end;
+
+implementation
+
+uses
+
+    NullModelResultSetImpl;
+
+    constructor TNullModelReader.create();
+    begin
+        fNullResultSet := TNullModelResultSet.create();
+    end;
+
+    destructor TNullModelReader.destroy();
+    begin
+        fNullResultSet := nil;
+        inherited destroy();
+    end;
+
+    (*!----------------------------------------------
+     * read data from storage
+     *-----------------------------------------------
+     * @param params parameter for search/filtering
+     * @return model data
+     *-----------------------------------------------*)
+    function TNullModelReader.read(const params : IModelParams = nil) : IModelResultSet;
+    begin
+        //intentionally does nothing
+        result := self;
+    end;
+
+    (*!----------------------------------------------
+     * return data instance after read() is execute
+     *-----------------------------------------------
+     * @return model data
+     *-----------------------------------------------*)
+    function TNullModelReader.data() : IModelResultSet;
+    begin
+        //intentionally does nothing
+        result := fNullResultSet;
+    end;
+
+end.
---
+++ src/Mvc/Models/Implementations/NullModelResultSetImpl.pas
@@ -0,0 +1,96 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit NullModelResultSetImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+type
+
+    (*!------------------------------------------------
+     * null model result set
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    TNullModelResultSet = class(TInterfacedObject, IModelResultSet)
+    public
+        (*!------------------------------------------------
+         * get total data
+         *-----------------------------------------------
+         * @return total data
+         *-----------------------------------------------*)
+        function count() : int64;
+
+        (*!------------------------------------------------
+         * test if in end of result set
+         *-----------------------------------------------
+         * @return true if no more record
+         *-----------------------------------------------*)
+        function eof() : boolean;
+
+        (*!------------------------------------------------
+         * move data pointer to next record
+         *-----------------------------------------------
+         * @return true if successful, false if no more record
+         *-----------------------------------------------*)
+        function next() : boolean;
+
+        (*!------------------------------------------------
+         * read data from current active record by its name
+         *-----------------------------------------------
+         * @return value in record
+         *-----------------------------------------------*)
+        function readString(const key : string) : string;
+    end;
+
+implementation
+
+    (*!------------------------------------------------
+     * get total data
+     *-----------------------------------------------
+     * @return total data
+     *-----------------------------------------------*)
+    function TNullModelResultSet.count() : int64;
+    begin
+        result := 0;
+    end;
+
+    (*!------------------------------------------------
+     * test if in end of result set
+     *-----------------------------------------------
+     * @return true if no more record
+     *-----------------------------------------------*)
+    function TNullModelResultSet.eof() : boolean;
+    begin
+        result := true;
+    end;
+
+    (*!------------------------------------------------
+     * move data pointer to next record
+     *-----------------------------------------------
+     * @return true if successful, false if no more record
+     *-----------------------------------------------*)
+    function TNullModelResultSet.next() : boolean;
+    begin
+        result := false;
+    end;
+
+    (*!------------------------------------------------
+     * read data from current active record by its name
+     *-----------------------------------------------
+     * @return value in record
+     *-----------------------------------------------*)
+    function TNullModelResultSet.readString(const key : string) : string;
+    begin
+        result := '';
+    end;
+
+end.
