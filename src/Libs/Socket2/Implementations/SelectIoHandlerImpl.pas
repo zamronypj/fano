88fed09cfbf637829552bb1d118757aacc93668a
Zamrony P. Juhara <zamronypj@yahoo.com>
2020-04-24 10:56:24 +0800

    improve read termination pipe

--- src/Libs/Socket2/Implementations/SelectIoHandlerImpl.pas
+++ src/Libs/Socket2/Implementations/SelectIoHandlerImpl.pas
@@ -295,7 +295,7 @@ uses
 
             if (clientSocket < 0) then
             begin
-                raiseExceptionIfAny();
+                handleAcceptError();
             end else
             begin
                 fSockOpts.makeNonBlocking(clientSocket);
@@ -334,7 +334,7 @@ uses
                 begin
                     //we get termination signal, just read until no more
                     //bytes and quit
-                    fpRead(pipeIn, @ch, 1);
+                    readPipe(pipeIn);
                     terminated := true;
                     break;
                 end else
