71bf7f5504e65dd1064c58daf1d430b372c6fe96
Zamrony P. Juhara <zamronypj@yahoo.com>
2020-05-30 23:54:48 +0800

    bump copyright year comment

--- tests/build.sh
+++ tests/build.sh
@@ -2,7 +2,7 @@
 # Fano Web Framework (https://fanoframework.github.io)
 #
 # @link      https://github.com/fanoframework/fano-cli
-# @copyright Copyright (c) 2018 Zamrony P. Juhara
+# @copyright Copyright (c) 2018 - 2020 Zamrony P. Juhara
 # @license   https://github.com/fanoframework/fano-cli/blob/master/LICENSE (MIT)
 #-------------------------------------------------------------
 #!/bin/bash
