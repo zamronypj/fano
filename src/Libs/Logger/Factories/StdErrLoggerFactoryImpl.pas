d1410cba7a7aef0dfd2a15fa3eda5a4e2f823b73
Zamrony P. Juhara <zamronypj@yahoo.com>
2021-01-20 07:36:08 +0800

    bump copyright year comment

--- src/Libs/Logger/Factories/StdErrLoggerFactoryImpl.pas
+++ src/Libs/Logger/Factories/StdErrLoggerFactoryImpl.pas
@@ -2,7 +2,7 @@
  * Fano Web Framework (https://fanoframework.github.io)
  *
  * @link      https://github.com/fanoframework/fano
- * @copyright Copyright (c) 2018 - 2020 Zamrony P. Juhara
+ * @copyright Copyright (c) 2018 - 2021 Zamrony P. Juhara
  * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
  *}
 unit StdErrLoggerFactoryImpl;
