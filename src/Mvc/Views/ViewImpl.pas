01f752bfdb8b2f8d64f8663b7d95445bfa8f3109
Zamrony P. Juhara <zamronypj@yahoo.com>
2021-02-22 11:24:52 +0700

    make TView internal field protected

--- src/Mvc/Views/ViewImpl.pas
+++ src/Mvc/Views/ViewImpl.pas
@@ -29,18 +29,18 @@ type
      * @author Zamrony P. Juhara <zamronypj@yahoo.com>
      *-------------------------------------------------*)
     TView = class(TInjectableObject, IView)
-    private
+    protected
 
         (*!------------------------------------------------
          * original HTML template content
          *-----------------------------------------------*)
-        templateContent : string;
+        fTemplateContent : string;
 
         (*!------------------------------------------------
          * helper class that will parse template content
          * and replace variable placeholder with value
          *-----------------------------------------------*)
-        templateParser : ITemplateParser;
+        fTemplateParser : ITemplateParser;
     public
 
         (*!------------------------------------------------
@@ -92,8 +92,8 @@ uses
     );
     begin
         inherited create();
-        templateParser := templateParserInst;
-        templateContent := tplContent;
+        fTemplateParser := templateParserInst;
+        fTemplateContent := tplContent;
     end;
 
     (*!------------------------------------------------
@@ -101,7 +101,7 @@ uses
      *-----------------------------------------------*)
     destructor TView.destroy();
     begin
-        templateParser := nil;
+        fTemplateParser := nil;
         inherited destroy();
     end;
 
@@ -120,7 +120,7 @@ uses
         contentLength : string;
     begin
         bodyInst := response.body();
-        bodyInst.write(templateParser.parse(templateContent, viewParams));
+        bodyInst.write(fTemplateParser.parse(fTemplateContent, viewParams));
         contentLength := intToStr(bodyInst.size());
         response.headers().setHeader('Content-Length',  contentLength);
         result := response;
