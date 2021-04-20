5f4188ac372ce7229b21105fadac7b238c2200d1
Zamrony P. Juhara <zamronypj@yahoo.com>
2021-04-20 08:35:12 +0700

    add base abstract mailer factory class

---
+++ src/Libs/Mailer/Implementations/Base/MailerConfigTypes.pas
@@ -0,0 +1,34 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 - 2021 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit MailerConfigTypes;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+type
+
+    (*!------------------------------------------------
+     * Data structure for SMTP server configuration
+     *
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    TMailerConfig = record
+        host : string;
+        port : word;
+        username : string;
+        password : string;
+        useTLS : boolean;
+        useSSL : boolean;
+        timeout : word;
+    end;
+
+implementation
+end.
--- src/Libs/Mailer/Implementations/Indy/Factories/IndyMailerFactoryImpl.pas
+++ src/Libs/Mailer/Implementations/Indy/Factories/IndyMailerFactoryImpl.pas
@@ -16,8 +16,8 @@ uses
 
     DependencyIntf,
     DependencyContainerIntf,
-    FactoryImpl,
-    IndyTypes;
+    AbstractMailerFactoryImpl,
+    MailerConfigTypes;
 
 type
 
@@ -26,17 +26,8 @@ type
      *
      * @author Zamrony P. Juhara <zamronypj@yahoo.com>
      *--------------------------------------------------*)
-    TIndyMailerFactory = class(TFactory, IDependencyFactory)
-    private
-        fConfig : TSmtpConfig;
+    TIndyMailerFactory = class(TAbstractMailerFactory)
     public
-        constructor create();
-
-        function host(const sHost : string) : TIndyMailerFactory;
-        function port(const sPort : word) : TIndyMailerFactory;
-        function username(const sUsername : string) : TIndyMailerFactory;
-        function password(const sPasw : string) : TIndyMailerFactory;
-
         function build(const container : IDependencyContainer) : IDependency; override;
     end;
 
@@ -45,38 +36,6 @@ implementation
 uses
 
     IndyMailerImpl;
-
-    constructor TIndyMailerFactory.create();
-    begin
-        fConfig := default(TSmtpConfig);
-        fConfig.host := 'localhost';
-        fConfig.port := 25;
-        fConfig.timeout := 5000;
-    end;
-
-    function TIndyMailerFactory.host(const sHost : string) : TIndyMailerFactory;
-    begin
-        fConfig.host := sHost;
-        result := self;
-    end;
-
-    function TIndyMailerFactory.port(const sPort : word) : TIndyMailerFactory;
-    begin
-        fConfig.port := sPort;
-        result := self;
-    end;
-
-    function TIndyMailerFactory.username(const sUsername : string) : TIndyMailerFactory;
-    begin
-        fConfig.username := sUsername;
-        result := self;
-    end;
-
-    function TIndyMailerFactory.password(const sPasw : string) : TIndyMailerFactory;
-    begin
-        fConfig.password := sPasw;
-        result := self;
-    end;
 
     function TIndyMailerFactory.build(const container : IDependencyContainer) : IDependency;
     begin
