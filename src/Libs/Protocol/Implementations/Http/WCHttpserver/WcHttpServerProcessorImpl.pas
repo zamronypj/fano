300ac10d543b5dbc875d3cabf8ada8bd5ea1a441
Zamrony P. Juhara <zamronypj@yahoo.com>
2021-04-21 17:19:30 +0700

    initial implementation

---
+++ src/Libs/Protocol/Implementations/Http/WCHttpserver/WcHttpServerProcessorImpl.pas
@@ -0,0 +1,167 @@
+{*!
+ * Fano Web Framework (https://fanoframework.github.io)
+ *
+ * @link      https://github.com/fanoframework/fano
+ * @copyright Copyright (c) 2018 - 2021 Zamrony P. Juhara
+ * @license   https://github.com/fanoframework/fano/blob/master/LICENSE (MIT)
+ *}
+
+unit MhdProcessorImpl;
+
+interface
+
+{$MODE OBJFPC}
+{$H+}
+
+uses
+
+    abstracthttpserver,
+    StreamAdapterIntf,
+    CloseableIntf,
+    RunnableIntf,
+    RunnableWithDataNotifIntf,
+    ProtocolProcessorIntf,
+    StreamIdIntf,
+    ReadyListenerIntf,
+    DataAvailListenerIntf,
+    EnvironmentIntf;
+
+type
+    (*!-----------------------------------------------
+     * Class which can process request from wchttpserver web server
+     *
+     * @link https://github.com/iLya2IK/wchttpserver
+     * @author Zamrony P. Juhara <zamronypj@yahoo.com>
+     *-----------------------------------------------*)
+    TWcHttpServerProcessor = class(TInterfacedObject, IProtocolProcessor, IRunnable, IRunnableWithDataNotif)
+    private
+        fStdIn : IStdIn;
+        fReadyListener : IReadyListener;
+        fDataListener : IDataAvailListener;
+    public
+        constructor create();
+
+        (*!------------------------------------------------
+         * process request stream
+         *-----------------------------------------------*)
+        function process(
+            const stream : IStreamAdapter;
+            const streamCloser : ICloseable;
+            const streamId : IStreamId
+        ) : boolean;
+
+        (*!------------------------------------------------
+         * get StdIn stream for complete request
+         *-----------------------------------------------*)
+        function getStdIn() : IStreamAdapter;
+
+        (*!------------------------------------------------
+         * set listener to be notified when request is ready
+         *-----------------------------------------------
+         * @return current instance
+         *-----------------------------------------------*)
+        function setReadyListener(const listener : IReadyListener) : IProtocolProcessor;
+
+        (*!------------------------------------------------
+         * get number of bytes of complete request based
+         * on information buffer
+         *-----------------------------------------------
+         * @return number of bytes of complete request
+         *-----------------------------------------------*)
+        function expectedSize(const buff : IStreamAdapter) : int64;
+
+        (*!------------------------------------------------
+         * run it
+         *-------------------------------------------------
+         * @return current instance
+         *-------------------------------------------------*)
+        function run() : IRunnable;
+
+        (*!------------------------------------------------
+         * set instance of class that will be notified when
+         * data is available
+         *-----------------------------------------------
+         * @param dataListener, class that wish to be notified
+         * @return true current instance
+         *-----------------------------------------------*)
+        function setDataAvailListener(const dataListener : IDataAvailListener) : IRunnableWithDataNotif;
+    end;
+
+implementation
+
+    constructor TWcHttpServerProcessor.create();
+    begin
+        fStdIn := nil;
+    end;
+
+    (*!------------------------------------------------
+     * process request stream
+     *-----------------------------------------------*)
+    function TWcHttpServerProcessor.process(
+        const stream : IStreamAdapter;
+        const streamCloser : ICloseable;
+        const streamId : IStreamId
+    ) : boolean;
+    begin
+        result := true;
+    end;
+
+    (*!------------------------------------------------
+     * get StdIn stream for complete request
+     *-----------------------------------------------*)
+    function TWcHttpServerProcessor.getStdIn() : IStreamAdapter;
+    begin
+        result := fStdIn;
+    end;
+
+    (*!------------------------------------------------
+     * set listener to be notified when request is ready
+     *-----------------------------------------------
+     * @return current instance
+     *-----------------------------------------------*)
+    function TWcHttpServerProcessor.setReadyListener(const listener : IReadyListener) : IProtocolProcessor;
+    begin
+        fReadyListener := listener;
+        result := self;
+    end;
+
+    (*!------------------------------------------------
+     * get number of bytes of complete request based
+     * on information buffer
+     *-----------------------------------------------
+     * @return number of bytes of complete request
+     *-----------------------------------------------*)
+    function TWcHttpServerProcessor.expectedSize(const buff : IStreamAdapter) : int64;
+    begin
+        result := 0;
+    end;
+
+    (*!------------------------------------------------
+     * run it
+     *-------------------------------------------------
+     * @return current instance
+     *-------------------------------------------------*)
+    function TWcHttpServerProcessor.run() : IRunnable;
+    var server : TAbsHTTPServer;
+    begin
+        server := TAbsHTTPServer.create(nil);
+        try
+            result := self;
+        finally
+            server.free();
+        end;
+    end;
+
+    (*!------------------------------------------------
+     * set instance of class that will be notified when
+     * data is available
+     *-----------------------------------------------
+     * @param dataListener, class that wish to be notified
+     * @return true current instance
+     *-----------------------------------------------*)
+    function TWcHttpServerProcessor.setDataAvailListener(const dataListener : IDataAvailListener) : IRunnableWithDataNotif;
+    begin
+        fDataListener := dataListener;
+        result := self;
+    end;
+end.
